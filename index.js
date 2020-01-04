5e870a7ef736ddd9b05e033a54e52c8c92dc4542
Rines.io <tsrines@gmail.com>
2020-01-03 19:46:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here
+let var1 = 8
+let var2 = 8
+const multiply = var1 * var2 - 2
+
+
+let random = Math.floor(Math.random() > 0);
+
+
+let mod = 10
+mod %= 6
+
+let max = Math.max(5, 10, 15, 20)
+ max
+
+
+
